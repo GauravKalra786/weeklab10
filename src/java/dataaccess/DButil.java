2d6000deac66f0fb637226732a51c2d712ef94f4
kalra <kalra@DESKTOP-TBG9J87>
2022-11-14 01:18:17 -0700

    Doing Lab10

--- src/java/dataaccess/DButil.java
+++ src/java/dataaccess/DButil.java
@@ -5,10 +5,14 @@
  */
 package dataaccess;
 
-/**
- *
- * @author kalra
- */
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DButil {
-    
+    private static final EntityManagerFactory emf =
+        Persistence.createEntityManagerFactory("NotesPU");
+
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
+    }
 }
